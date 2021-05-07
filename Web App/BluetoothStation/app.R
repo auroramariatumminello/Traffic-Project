cb1774f17a3e690a5d2862c8432dee0035221b24
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-05-07 14:44:40 +0200

    Updated application

--- Web App/BluetoothStation/app.R
+++ Web App/BluetoothStation/app.R
@@ -4,20 +4,30 @@ library(leaflet)
 #library(dplyr)
 library(leaflet.extras)
 #import data
+#setwd("G:/Il mio Drive/First Year/Big Data Technologies/Traffic Project/Web App/BluetoothStation")
 data <- read.csv("BluetoothStations.csv")
-
 # Define UI for application that draws a histogram
-ui <- bootstrapPage(
-    tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-    leafletOutput("map", width = "100%", height = "100%"),
-    
+ui <- fluidPage(
+    tags$style(type = "text/css", "html, body, #map {padding:5px;width:100%;height:100%}"),
+    selectInput("station", 
+                "Select a station",
+                choices = c("All",data$name)),
+    leafletOutput(outputId = "map",height=700),
 )
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+    
+    selected_station = reactive({
+        df = data
+        if(input$station!='All'){
+            df = data[data$name==input$station,]
+        }
+        df
+    })
 
     output$map <- renderLeaflet({
         leaflet(data) %>% 
-            setView(lng = 11.2, lat = 46.6, zoom = 9)  %>%
+            setView(lng = 11.2, lat = 46.40, zoom =9)  %>%
             addTiles() %>% 
             addMarkers(data = data, 
                        lat = ~ lat,
