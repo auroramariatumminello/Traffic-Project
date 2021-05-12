8b3c6efae8872393a3006e0a2889dea520204ad1
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-05-12 19:30:16 +0200

    Handling exceptions and saving key for EC2

--- py/download_real_time.py
+++ py/download_real_time.py
@@ -9,15 +9,8 @@ import os.path
 import time
 import pytz
 import tqdm
-
-# Importing scripts with objects
-from BluetoothStation import *
-from Database_Manager import *
-#%%
-# Creating a MySQL Database Manager
-manager = MySQLStationManager("Aurora")
+from datetimerange import DateTimeRange
 # %%
-
 # URL to request vehicle detection in Bluetooth Stations
 data_url = "https://mobility.api.opendatahub.bz.it/v2/flat%2Cnode/BluetoothStation/%2A/{}/{}?limit=-1&distinct=true&timezone=UTC"
 data_format = "{}-{}-{}T{}%3A{}%3A{}.000%2B0000"  # YYYY-mm-ddTHH:MM:SS
@@ -25,8 +18,6 @@ data_format = "{}-{}-{}T{}%3A{}%3A{}.000%2B0000"  # YYYY-mm-ddTHH:MM:SS
 # %%
 def get_data_of_day(url, sdate, edate, filename=None):
     try:
-        #if (dt.datetime(2019, 11, 26, tzinfo=pytz.UTC) <= sdate <= dt.datetime.now(tz=pytz.UTC)) or (dt.datetime(2019, 11, 26, tzinfo=pytz.UTC) <= edate <= dt.datetime.now(tz=pytz.UTC)):
-        #    # For limited requests (every hour)
         req = requests.get(url.format(data_format.format(sdate.year, 
                                                              sdate.strftime("%m"), 
                                                              sdate.strftime("%d"),
@@ -42,13 +33,6 @@ def get_data_of_day(url, sdate, edate, filename=None):
         
         day_data = req.json()["data"]
         results = from_json_to_list(day_data)
-        results = results
-        if filename != None:
-            df.to_cv(filename,
-                     mode='a',
-                     index=False,
-                     header=False)
-        return results
     except ValueError:
         # Possible error of gateway, retry after 1 minute
         print("Gateway Time-out error")
@@ -60,7 +44,7 @@ def from_json_to_list(json_data: json):
     result = []
     for element in json_data:
         if element['tname'] == "Bluetooth Count record" and element['ttype'] == 'Count':
-            m = [datetime.strptime(element['mvalidtime'][:19], '%Y-%m-%d %H:%M:%S'),
+            m = [dt.strptime(element['mvalidtime'][:19], '%Y-%m-%d %H:%M:%S'),
                  element['mvalue'],
                  element['sname']]
             result.append(m)
@@ -68,27 +52,17 @@ def from_json_to_list(json_data: json):
 
 
 #%%
-# DATE RANGES
-
-# Date range from 01-01-2013 to 26-11-2019 (in days)
-old_date_range = DateTimeRange("2021-04-01T00:00:00.000+0000","2021-12-31T00:00:00.000+0000")
-old_date_range = [value for value in old_date_range.range(dt.timedelta(hours=1))]
-
-# Datetime range from 26-11-2019 00:00:00 to nowadays (in hours)
-new_date_range = DateTimeRange("2019-11-26T00:00:00.000+0000",
-                               "2020-01-01T00:00:00.000+0000")
-new_date_range = [value for value in new_date_range.range(dt.timedelta(hours=1))]
-
 # %%
 #   TODO: Progress bar
 def get_data_in_range(date_range,url=data_url):
-    manager = MySQLStationManager("Aurora")
+    db = MySQLStationManager("Aurora")
     for i in tqdm.trange(len(date_range)-1):
         sdate = date_range[i]
         print("Saving "+str(sdate))
         edate = date_range[i+1]
-        manager.insert_measurements(get_data_of_day(url, sdate, edate))
+        for row in get_data_of_day(url, sdate, edate):
+            db.putItem(row)
 
 # %%
-get_data_in_range(old_date_range)
+get_data_in_range(date_range)
 # %%
