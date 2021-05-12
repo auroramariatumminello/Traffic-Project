72af74105c9e41fe73b901e5f719d3109ff20a55
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-05-12 22:41:42 +0200

    Created remote MySQL DB
    Testing the introduction of a new Manager, to directly insert/remove/query the remote DB, but it is quite hard due to the SSH Tunneling

--- py/Database_Manager.py
+++ py/Database_Manager.py
@@ -98,3 +98,99 @@ class MySQLStationManager:
         print("Finished.")
         cursor.close()
         return stations
+    
+#%%
+import sshtunnel
+#%%
+class MySQLStationManagerAWS:
+    
+    def __init__(self):
+        key = open('../db/BigDataProject.pem').readlines()
+        with sshtunnel.SSHTunnelForwarder(('ec2-35-156-254-73.eu-central-1.compute.amazonaws.com'),
+                                          ssh_username='ubuntu',
+                                          ssh_pkey=key,
+                                          remote_bind_address=('root', 3306)) as tunnel:
+            self.connection = mysql.connector.connect(
+                host="127.0.0.1",
+                user="root",
+                password="BigData",
+                port=3306,
+                database="bluetoothstations",
+                auth_plugin='mysql_native_password')
+
+    def disconnect(self):
+        self.connection.close()
+
+    # Need to update: ask whether the station is already in the station table
+    def insert_measurements(self, observations: List[Measurement]):
+        # If the station is in BluetoothStation table
+        cursor = self.connection.cursor()
+        query = "INSERT IGNORE into measurement (timestamp, count, station) VALUES (%s, %s, %s)"
+        try:
+            for obs in observations:
+                cursor.execute(query, (
+                    obs.timestamp.strftime("%Y-%m-%d %H:%M:%S"),
+                    obs.count,
+                    obs.station.name
+                ))
+            # print("Data inserted.")
+            cursor.close()
+        except mysql.connector.errors.IntegrityError:
+            pass
+         
+    def insert_stations(self, stations: List[BluetoothStation]):
+        cursor = self.connection.cursor()
+        query = "INSERT into station (name, latitude, longitude) VALUES (%s, %s, %s)"
+
+        for station in stations:
+            if station.coords.lat == None or station.coords.lon == None:
+                cursor.execute(
+                    query, (
+                    station.name,
+                    None,
+                    None))
+            else:
+                cursor.execute(query, (
+                    station.name,
+                    station.coords.lat,
+                    station.coords.lon
+                ))
+        # print("Data inserted.")
+        cursor.close()
+
+    # Generalize by inserting optional parameter about the station(s)
+    def list_all_measurement(self):
+        cursor = self.connection.cursor()
+        query = "SELECT * from measurement"
+        cursor.execute(query)
+        print("Got all measurements.")
+        measurements = []
+        for timestamp, count, station in cursor:
+            measurements.append(Measurement(
+                timestamp,
+                count,
+                station
+            ))
+        print("Finished.")
+        cursor.close()
+        return measurements
+    
+    # List of all BluetoothStations in the database
+    def list_all_stations(self):
+        cursor = self.connection.cursor()
+        query = "SELECT * from station"
+        cursor.execute(query)
+
+        print("Got all measurements.")
+        stations = []
+        for name, latitude, longitude in stations:
+            stations.append(BluetoothStation(
+                name,
+                Position(latitude, longitude)
+            ))
+        print("Finished.")
+        cursor.close()
+        return stations
+
+#%%
+MySQLStationManagerAWS()
