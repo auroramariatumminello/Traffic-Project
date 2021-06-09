8434f132ffb994dbab9e7dceacf0b4d013be6e54
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-06-09 14:25:52 +0200

    Changed requirements and connection established

--- Web App/trial/mysql_connection.R
+++ Web App/trial/mysql_connection.R
@@ -1,16 +1,23 @@
 library(odbc)
 library(RMySQL)
 library(config)
+library(tidyverse)
 
+
+setwd("C:/Users/auror/OneDrive/Documenti/GitHub/traffic/Traffic Project/Web App/trial")
 config <- config::get(file = "config.yml")
-con <- RMySQL::dbConnect(
+print(config)
+mydb <- RMySQL::dbConnect(
   RMySQL::MySQL(),
-  user = config$db_user,
-  password = config$db_password,
-  dbname = config$db_name,
-  host = config$db_host
+  user = "marshall",
+  password = "happyslashgiving",
+  dbname = "bluetoothstations",
+  host = "traffic-db.ce2ieg6xrefy.us-east-2.rds.amazonaws.com"
 )
 
-df <- con %>%
-  dplyr::tbl("station") %>%
-  dplyr::collect()
+query = paste("
+              SELECT *
+              FROM bluetoothstations.measurement
+              ORDER BY timestamp DESC")
+rs = dbSendQuery(mydb,query)
+rs
