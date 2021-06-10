f89cc1d1ae06906aeb3b745c539e5c9158534251
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-06-10 22:48:33 +0200

    Map of stations

--- Web App/trial/mysql_connection.R
+++ Web App/trial/mysql_connection.R
@@ -1,15 +1,18 @@
 library(odbc)
 library(RMySQL)
 library(config)
+library("RMariaDB")
 library(tidyverse)
 
-setwd("C:/Users/auror/OneDrive/Documenti/GitHub/traffic/Traffic Project/Web App/trial")
 config <- config::get(file = "config.yml")
-print(config)
-mydb <- RMySQL::dbConnect(
-  RMySQL::MySQL(),
-  user = "marshall",
-  password = "happyslashgiving",
-  dbname = "bluetoothstations",
-  host = "traffic-db.ce2ieg6xrefy.us-east-2.rds.amazonaws.com"
-)
+
+dbconnect <- function(){ 
+  mydb <- RMariaDB::dbConnect(
+    RMariaDB::MariaDB(),
+    user = "marshall",
+    password = "happyslashgiving",
+    dbname = "bluetoothstations",
+    host = "traffic-db.ce2ieg6xrefy.us-east-2.rds.amazonaws.com"
+  )
+  return(mydb)
+}
