8c559418a4ca67ff4e023aa2da2d0de1474a41a4
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-07-15 18:16:29 +0200

    Update model requirements

--- py/Traffic_Prediction.py
+++ py/Traffic_Prediction.py
@@ -2,20 +2,13 @@
 # Libraries
 
 # Model
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import mean_squared_error
 from tensorflow import keras
 
 # Data Transformation
-from datetime import datetime, timedelta
+from datetime import timedelta
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from datetimerange import DateTimeRange
 
 import Database_Manager
 # %%
