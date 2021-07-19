a693bea20378e133a648703ab8d6cc988be281c7
AurorArctic <aurora.maria.tumminello.z@gmail.com>
2021-07-19 17:49:56 +0200

    Cleaning database scripts

--- db/traffic_per_month.sql
+++
@@ -1,4 +0,0 @@
-CREATE TEMPORARY TABLE bluetoothstations.month_traffic
-SELECT MONTH(timestamp) as month,SUM(count)
-FROM bluetoothstations.measurement
-GROUP BY month(timestamp);
--- py/download_history.py
+++ py/download_history.py
@@ -40,7 +40,7 @@ def get_stations_details(url: str = "https://mobility.api.opendatahub.bz.it/v2/f
 
 # %%
 # Inserting the requested stations inside the station table
-# manager.insert_stations(get_stations_details())
+manager.insert_stations(get_stations_details())
 # %%
 
 # URL to request vehicle detection in Bluetooth Stations
@@ -48,6 +48,7 @@ data_url = "https://mobility.api.opendatahub.bz.it/v2/flat%2Cnode/BluetoothStati
 data_format = "{}-{}-{}T{}%3A{}%3A{}.000%2B0000"  # YYYY-mm-ddTHH:MM:SS
 
 # %%
+# Function that requests data from sdate to edate
 def get_data_of_day(url, sdate, edate, filename=None):
     try:
         start = time.time()
@@ -77,7 +78,7 @@ def get_data_of_day(url, sdate, edate, filename=None):
         results = [Measurement(df.iloc[i, 0],
                                int(df.iloc[i, 2]),
                                BluetoothStation(df.iloc[i, 1])) for i in range(len(df))]
-        # print(time.time()-start)
+
         if filename != None:
             df.to_cv(filename,
                      mode='a',
@@ -90,7 +91,7 @@ def get_data_of_day(url, sdate, edate, filename=None):
         time.sleep(60)
         get_data_of_day(url, sdate, edate, filename)
 
-
+# Converts json to list of values
 def from_json_to_list(json_data: json):
     result = []
     for element in json_data:
@@ -113,13 +114,14 @@ def from_json_to_measurement(json_data: json):
     return result
 
 #%%
-# DATE RANGE
+# DATE RANGE that varies within an hour
 date_range = DateTimeRange("2021-07-10T05:30:00.000+0000",
                            "2021-07-10T16:00:00.000+0000",)
 date_range = [value for value in date_range.range(dt.timedelta(hours=1))]
 
 
 # %%
+# Main function that gathers data within the date range and inserts them inside the DB
 def get_data_in_range(date_range,url=data_url): 
     manager = MySQLStationManagerAWS()
     for i in tqdm(range(len(date_range)-1)):
